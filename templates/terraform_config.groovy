44f94c14f9c00430864f8293ed1b79cc6976fec5
Mostafa Gheta <mostafagheta75@gmail.com>
2026-08-26 02:11:06 +0300

    Infrastructure jte changes

--- templates/terraform_config.groovy
+++ templates/terraform_config.groovy
@@ -1,12 +1,11 @@
 /*
   JTE Global Configuration
-  Library parameters live here (first config in the chain) so they are not
-  stripped when the application pipeline_config.groovy is merged.
 */
 jte {
     pipeline_template = "templates/terraform_pipeline"
 }
-@merge libraries {
+
+libraries {
     s3_versioning {
         s3_bucket = "atos-versioning-bucket"
         s3_version_file = "infra_version.json"
@@ -14,4 +13,3 @@ jte {
     infra
     common
 }
-
