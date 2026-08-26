b338e9cb3a81bc7f01c1e028d01445aff67def7a
Mostafa Gheta <mostafagheta75@gmail.com>
2026-08-27 01:51:21 +0300

    jte ansible lib edit

--- libraries/ansible/steps/ansibleSyntax.groovy
+++ libraries/ansible/steps/ansibleSyntax.groovy
@@ -5,19 +5,13 @@ def call() {
     echo "========================================"
 
     sshagent(credentials: ['mykey']) {
-
-        sh '''
-            set -e
-
-            ansible bastion \
-                -i inventory.ini \
-                -m ping
-
-            ansible bastion \
-                -i inventory.ini \
-                -m shell \
-                -a "hostname"
-        '''
+    withEnv(['ANSIBLE_HOST_KEY_CHECKING=False']) {
+    sh '''
+        set -e
+        ansible bastion -i inventory.ini -m ping
+        ansible bastion -i inventory.ini -m shell -a "hostname"
+    '''
+    }
     }
 
     echo "Ansible verification completed successfully."
