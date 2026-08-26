6f36b85273abf8a416923f27a8936385cd7fb034
Mostafa Gheta <mostafagheta75@gmail.com>
2026-08-27 01:23:55 +0300

    jte ansible lib edit

--- libraries/ansible/steps/fetchInventory.groovy
+++ libraries/ansible/steps/fetchInventory.groovy
@@ -1,13 +1,12 @@
 void call(Map config = [:]) {
-    // Accept selector from pipeline or use default
-    def selector = config.get('selector', lastBuild())
-    def projectName = config.get('projectName', 'infra')
-    def filter = config.get('filter', 'inventory.ini, cluster-autoscaler.yaml')
+    // Get build number from config or use default
+    def buildNumber = config.buildNumber ?: 'last-successful'
+    def projectName = config.projectName ?: 'infra'
     
     copyArtifacts(
         projectName: projectName,
-        selector: selector,
-        filter: filter,
+        selector: specific(buildNumber),
+        filter: config.filter ?: 'inventory.ini, cluster-autoscaler.yaml',
         fingerprintArtifacts: true
     )
 }
