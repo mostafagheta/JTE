62857bd8a3e1cea7054b87015a27e5bf3ea0e354
Mostafa Gheta <mostafagheta75@gmail.com>
2026-08-27 02:08:39 +0300

    jte ansible library edit

--- libraries/ansible/steps/ansibleSyntax.groovy
+++ libraries/ansible/steps/ansibleSyntax.groovy
@@ -4,16 +4,19 @@ def call() {
     echo "Ansible Verification"
     echo "========================================"
 
-
-        withCredentials([file(credentialsId: 'mykey', variable: 'SSH_KEY')]) {
+    withCredentials([sshUserPrivateKey(
+        credentialsId: 'mykey', 
+        keyFileVariable: 'SSH_KEY',
+        usernameVariable: 'SSH_USER'
+    )]) {
         sh '''
             set -e
 
             # Set key permissions
             chmod 600 $SSH_KEY
 
-        ansible bastion -i inventory.ini -m ping --private-key $SSH_KEY
-        ansible bastion -i inventory.ini -m shell -a "hostname" --private-key $SSH_KEY
+            ansible bastion -i inventory.ini -m ping --private-key $SSH_KEY 
+            ansible bastion -i inventory.ini -m shell -a "hostname" --private-key $SSH_KEY 
         '''
     }
 
