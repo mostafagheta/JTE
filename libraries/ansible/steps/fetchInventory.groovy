e778644b58009eee7c8c8c05a2bec58134694252
Mostafa Gheta <mostafagheta75@gmail.com>
2026-08-27 01:18:13 +0300

    jte ansible lib edit

--- libraries/ansible/steps/fetchInventory.groovy
+++ libraries/ansible/steps/fetchInventory.groovy
@@ -1,8 +1,14 @@
-// fetchInventory/fetchInventory.groovy
 void call(Map config = [:]) {
+    // Import Jenkins model if needed
+    import jenkins.model.Jenkins
+    
+    // Get the last successful build of the project
+    def job = Jenkins.instance.getItem(config.projectName)
+    def lastSuccessfulBuild = job?.lastSuccessfulBuild
+    
     copyArtifacts(
         projectName: config.projectName,
-        selector: config.get('selector', lastSuccessful()),
+        selector: config.get('selector', lastSuccessfulBuild),  // Use property, not method
         filter: config.get('filter', 'inventory.ini, cluster-autoscaler.yaml'),
         fingerprintArtifacts: true
     )
