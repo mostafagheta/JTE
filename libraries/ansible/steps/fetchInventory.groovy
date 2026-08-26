b853923e3d2d733d12050ec58538bbeae1468399
Mostafa Gheta <mostafagheta75@gmail.com>
2026-08-27 01:43:43 +0300

    jte ansible lib edit

--- libraries/ansible/steps/fetchInventory.groovy
+++ libraries/ansible/steps/fetchInventory.groovy
@@ -1,11 +1,14 @@
-def call() {
-    // Use copyArtifacts to fetch the inventory files
+def call(Map config = [:]) {
+    def projectName = config.projectName ?: 'infra'
+    def buildNumber  = config.buildNumber ?: 'last-successful'
+    def filter       = config.filter ?: 'inventory.ini, cluster-autoscaler.yaml'
+
     copyArtifacts(
-        projectName: 'infra',
-        selector: 'last-successful',
-        filter: 'inventory.ini, cluster-autoscaler.yaml',
+        projectName: projectName,
+        selector: buildNumber == 'last-successful' ? lastSuccessful() : specific(buildNumber),
+        filter: filter,
         fingerprintArtifacts: true
     )
-    
-    echo "Inventory files fetched successfully from 'infra' job"
+
+    echo "Inventory files fetched successfully from '${projectName}' job"
 }
