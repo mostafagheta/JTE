138da990b0a421f330238e09e52434901c79c39a
Mostafa Gheta <mostafagheta75@gmail.com>
2026-08-27 05:19:23 +0300

    jte change

--- libraries/ansible/steps/ansibleCheck.groovy
+++ libraries/ansible/steps/ansibleCheck.groovy
@@ -10,12 +10,21 @@ def call() {
             set -e
 
             export ANSIBLE_ROLES_PATH=/home/jenkins/workspace/ansible/ansible/roles
-            ansible bastion -i inventory.ini -m raw -a "sudo yum install -y python3-pip" --become
+            ansible bastion -i inventory.ini -m raw -a "sudo yum install -y python3-pip" --become 
+            set +e
             ansible-playbook \
                 -i inventory.ini \
                 ansible/playbooks/install_addons.yml \
                 --check \
                 --diff
+            CHECK_EXIT=$?
+            set -e
+
+            if [ $CHECK_EXIT -ne 0 ]; then
+                echo "WARNING: Ansible check mode reported issues (exit code $CHECK_EXIT). Review diff output above."
+            else
+                echo "Ansible check mode passed with no issues."
+            fi
         '''
     }
 
