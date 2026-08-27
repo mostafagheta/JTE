7d4e864ad75af3ce6c68eeb2efcc5b39529d2bb9
Mostafa Gheta <mostafagheta75@gmail.com>
2026-08-27 04:56:50 +0300

    jte change

--- libraries/ansible/steps/ansibleSyntax.groovy
+++ libraries/ansible/steps/ansibleSyntax.groovy
@@ -4,13 +4,23 @@ def call() {
     echo "Ansible Verification"
     echo "========================================"
 
-        sshagent(credentials: ['mykey']) {
+    sshagent(credentials: ['mykey']) {
 
         sh '''
             set -e
 
-            ansible bastion -i inventory.ini -m ping 
-            ansible bastion -i inventory.ini -m shell -a "hostname" 
+            mkdir -p ~/.ssh
+            chmod 700 ~/.ssh
+
+            BASTION_HOST=$(ansible-inventory -i inventory.ini --host bastion | grep -m1 '"ansible_host"' | cut -d'"' -f4)
+            if [ -z "$BASTION_HOST" ]; then
+                BASTION_HOST=$(awk '/^\\[bastion\\]/{getline; print $1; exit}' inventory.ini)
+            fi
+
+            ssh-keyscan -H "$BASTION_HOST" >> ~/.ssh/known_hosts 2>/dev/null
+
+            ansible bastion -i inventory.ini -m ping
+            ansible bastion -i inventory.ini -m shell -a "hostname"
         '''
     }
 
