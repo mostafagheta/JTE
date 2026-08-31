c4dc7145e8acb5995117651ed7b6476b04a4929b
Mostafa Gheta <mostafagheta75@gmail.com>
2026-08-31 13:12:57 +0300

    jte edit

--- libraries/ansible/steps/ansibleSyntax.groovy
+++ libraries/ansible/steps/ansibleSyntax.groovy
@@ -1,28 +1,17 @@
 def call() {
 
     echo "========================================"
-    echo "Ansible Verification"
+    echo "Ansible Syntax Check"
     echo "========================================"
 
-    sshagent(credentials: ['mykey']) {
+    sh '''
+        set -e
 
-        sh '''
-            set -e
+        ansible-playbook \
+            -i inventory.ini \
+             ansible/playbooks/install_addons.yml \
+            --syntax-check
+    '''
 
-            mkdir -p ~/.ssh
-            chmod 700 ~/.ssh
-
-            BASTION_HOST=$(ansible-inventory -i inventory.ini --host bastion | grep -m1 '"ansible_host"' | cut -d'"' -f4)
-            if [ -z "$BASTION_HOST" ]; then
-                BASTION_HOST=$(awk '/^\\[bastion\\]/{getline; print $1; exit}' inventory.ini)
-            fi
-
-            ssh-keyscan -H "$BASTION_HOST" >> ~/.ssh/known_hosts 2>/dev/null
-
-            ansible bastion -i inventory.ini -m ping
-            ansible bastion -i inventory.ini -m shell -a "hostname"
-        '''
-    }
-
-    echo "Ansible verification completed successfully."
+    echo "Ansible syntax check completed successfully."
 }
